c2b2f739a1cc1944e3619ca69a8cb7dc67a836a1
Cooper Saye <csaye2022@chadwickschool.org>
2020-04-08 22:58:30 -0700

    Add main menu.

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    
+    public string destinationScene;
+
+    public Sprite buttonNormal;
+    public Sprite buttonHighlight;
+    
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        CheckHighlight();
+    }
+
+    // Checks if the mouse is over the button, and if so, highlights the button
+    void CheckHighlight() {
+
+        // Raycast to mouse position
+        RaycastHit2D rayHit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
+        
+        // If button hit by raycast, highlight button
+        if (rayHit.collider != null && transform.position == rayHit.collider.gameObject.transform.position) {
+            spriteRenderer.sprite = buttonHighlight;
+
+            // If highlighted and mouse button down, activate scene switching
+            if (Input.GetMouseButtonDown(0)) SwitchScene();
+
+        } else {
+            spriteRenderer.sprite = buttonNormal;
+        }
+    }
+
+    void SwitchScene() {
+        SceneManager.LoadScene(destinationScene);
+    }
+}
